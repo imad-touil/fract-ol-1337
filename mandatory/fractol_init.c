e8f1eb6377fd9c7840aa94d3b718463944901b06
imad-touil <152611727+imad-touil@users.noreply.github.com>
2025-03-08 00:35:39 +0000

    fract-ol 1337

--- mandatory/fractol_init.c
+++ mandatory/fractol_init.c
@@ -6,39 +6,24 @@
 /*   By: imatouil <imatouil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/23 11:27:42 by imatouil          #+#    #+#             */
-/*   Updated: 2025/02/23 13:12:29 by imatouil         ###   ########.fr       */
+/*   Updated: 2025/02/24 15:19:54 by imatouil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../fractol.h"
 
-void	print_error(void)
+void	fractol_init(t_data *f)
 {
-	perror("malloc eroor\n");
-	exit(EXIT_FAILURE);
-}
-
-void	fractol_init(t_fractol *f)
-{
-	f -> mlx = mlx_init();
-	if (f -> mlx == NULL)
-		print_error();
-	f -> mlx_win = mlx_new_window(f -> mlx, WIDTH, HEIGHT, f -> name);
-	if (f -> mlx_win == NULL)
-	{
-		mlx_destroy_window(f -> mlx, f -> mlx_win);
-		free(f -> mlx);
-		print_error();
-	}
-	f -> img.img_p = mlx_new_image(f -> mlx, WIDTH, HEIGHT);
-	if (f -> img.img_p == NULL)
-	{
-		mlx_destroy_image(f ->mlx, f -> img.img_p);
-		mlx_destroy_window(f -> mlx, f -> mlx_win);
-		free(f -> mlx);
-		print_error();
-	}
-	f -> img.pix_p = mlx_get_data_addr(f->img.img_p, &f->img.bit_per_pix,
-			&f->img.line_len, &f->img.endian);
-	mlx_loop(f -> mlx);
+	f->mlx = mlx_init();
+	if (!f-> mlx)
+		write(1, "TODO: write faunction that handle this", 37); // Free windo and images and exit 
+	f -> win = mlx_new_window(f->mlx, WIDTH, HEIGHT, f->title);
+	if (!f->win)
+		write(1, "TODO: write faunction that handle this\n", 37);
+	f->img = mlx_new_image(f->mlx, WIDTH, HEIGHT);
+	if (!f->img)
+		write(1, "TODO: write faunction that handle this\n", 37);
+	f->addr = mlx_get_data_addr(f->img, &f->bpp, &f->l_len, &f->endian);
+	// event_init(f); //TODO
+	// data_init(f); //TODO
 }
--- mandatory/operations.c
+++ mandatory/operations.c
@@ -1,39 +1,39 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   operations.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: imatouil <imatouil@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/21 17:11:29 by imatouil          #+#    #+#             */
-/*   Updated: 2025/02/21 20:49:26 by imatouil         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+// /* ************************************************************************** */
+// /*                                                                            */
+// /*                                                        :::      ::::::::   */
+// /*   operations.c                                       :+:      :+:    :+:   */
+// /*                                                    +:+ +:+         +:+     */
+// /*   By: imatouil <imatouil@student.42.fr>          +#+  +:+       +#+        */
+// /*                                                +#+#+#+#+#+   +#+           */
+// /*   Created: 2025/02/21 17:11:29 by imatouil          #+#    #+#             */
+// /*   Updated: 2025/02/21 20:49:26 by imatouil         ###   ########.fr       */
+// /*                                                                            */
+// /* ************************************************************************** */
 
-#include "../fractol.h"
+// #include "../fractol.h"
 
-t_complex	complex_square(t_complex z)
-{
-	t_complex	result;
+// t_complex	complex_square(t_complex z)
+// {
+// 	t_complex	result;
 
-	result.real = z.real * z.real - z.imagi * z.imagi;
-	result.imagi = 2 * z.real * z.imagi;
-	return (result);
-}
+// 	result.real = z.real * z.real - z.imagi * z.imagi;
+// 	result.imagi = 2 * z.real * z.imagi;
+// 	return (result);
+// }
 
-t_complex	complex_add(t_complex z1, t_complex z2)
-{
-	t_complex	result;
+// t_complex	complex_add(t_complex z1, t_complex z2)
+// {
+// 	t_complex	result;
 
-	result.real = z1.real + z2.real;
-	result.imagi = z1.imagi + z2.imagi;
-	return (result);
-}
+// 	result.real = z1.real + z2.real;
+// 	result.imagi = z1.imagi + z2.imagi;
+// 	return (result);
+// }
 
-double	complex_magnitude(t_complex z)
-{
-	double	res;
+// double	complex_magnitude(t_complex z)
+// {
+// 	double	res;
 
-	res = sqrt(z.real * z.real + z.imagi * z.imagi);
-	return (res);
-}
+// 	res = sqrt(z.real * z.real + z.imagi * z.imagi);
+// 	return (res);
+// }
